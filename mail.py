d52f9c3b67f3eab2bfbb2885965d5d44f1e24e80
Anthony An <dstdaskjl@gmail.com>
2021-05-21 12:26:56 -0400

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,9 +1,9 @@
 import smtplib
 
 class Mail:
-    def send(self, sender, password, receiver, subject, message):
+    def send(self, host, port, sender, password, receiver, subject, message):
         try:
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server = smtplib.SMTP_SSL(host, port)
             server.ehlo()
             server.login(sender, password)
             server.sendmail(sender, receiver, 'Subject: {}\n\n{}'.format(subject, message))
@@ -14,3 +14,6 @@ class Mail:
 
         finally:
             del server, sender, receiver, subject, message
+
+    def send_gmail(self, sender, password, receiver, subject, message):
+        self.send('smtp.gmail.com', 465, sender, password, receiver, subject, message)
