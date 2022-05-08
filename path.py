7a9bd79b41cb777370848a257b690cad234fb2d9
dstdaskjl <55066249+dstdaskjl@users.noreply.github.com>
2022-05-08 12:55:49 -0400

    Removed unused functions

--- path.py
+++ path.py
@@ -1,46 +1,19 @@
-import collections
-import json
 import os
-import pathlib
 import shutil
-import sys
+import pathlib
+import json
+import traceback
 from zipfile import ZipFile
 
-from log import Log
 
-
-logger = Log()
-
-def args_valid(func):
+def handle_exception(func):
     def decorate(*args, **kwargs):
-        if not 'path' in kwargs:
-            logger.error('Pass a path using kwargs')
-        return func(*args, **kwargs)
+        try:
+            return func(*args, **kwargs)
+        except Exception:
+            print(traceback.format_exc())
     return decorate
 
-def exists(func):
-    @args_valid
-    def decorate(*args, **kwargs):
-        if not Path().exists(path=kwargs['path']):
-            logger.error(kwargs['path'] + ' does not exist')
-        return func(*args, **kwargs)
-    return decorate
-
-def is_file(func):
-    @exists
-    def decorate(*args, **kwargs):
-        if not Path().is_file(kwargs['path']):
-            logger.error(kwargs['path'] + ' is not a file')
-        return func(*args, **kwargs)
-    return decorate
-
-def is_dir(func):
-    @exists
-    def decorate(*args, **kwargs):
-        if not Path().is_dir(kwargs['path']):
-            logger.error(kwargs['path'] + ' is not a directory')
-        return func(*args, **kwargs)
-    return decorate
 
 def apply_to_all(func):
     def decorate(cls):
@@ -51,174 +24,65 @@ def apply_to_all(func):
     return decorate
 
 
+@apply_to_all(handle_exception)
 class Path:
-    def create(self, path):
-        if self.is_dir(path=path):
-            os.makedirs(path)
-        elif self.is_file(path=path):
-            dirname = File().get_dirname(path=path)
-            if not self.exists(dirname):
-                os.makedirs(path)
-            with open(path, 'w'): pass
+    def copy_file(self, src: str, dst: str) -> None:
+        shutil.copyfile(src, dst)
+
+    def create(self, *args: str) -> None:
+        # File
+        if '.' in args[-1]:
+            open(self.join(*args), 'w')
+        # Directory
         else:
-            logger.error(path + ' is neither a directory nor file')
+            os.mkdir(self.join(*args))
 
-    def exists(self, path: str) -> bool:
-        return os.path.exists(path)
+    def delete(self, *args: str) -> None:
+        # File
+        if '.' in args[-1]:
+            os.remove(self.join(*args))
+        # Directory
+        else:
+            shutil.rmtree(self.join(*args))
 
-    def home_path(self) -> str:
+    def exists(self, *args: str) -> bool:
+        return os.path.exists(self.join(*args))
+
+    def home(self) -> str:
         return str(pathlib.Path.home())
 
-    def is_dir(self, path: str) -> bool:
-        return os.path.isdir(path)
+    def is_empty(self, *args: str) -> bool:
+        return len(self.listdir(*args)) == 0
 
-    def is_file(self, path: str) -> bool:
-        return os.path.isfile(path)
-
-    def join(self, *args):
+    def join(self, *args: str) -> str:
         return os.path.join(*args)
 
+    def list_dir(self, *args: str) -> list:
+        return os.listdir(self.join(*args))
 
-@apply_to_all(is_file)
-class Compression:
-    def zip(self, path: str, dest: str):
-        try:
-            with ZipFile(dest, 'w') as z:
-                z.write(path)
-        except Exception as e:
-            logger.error(e)
+    def read_text(self, filepath: str) -> list:
+        with open(filepath, 'r') as f:
+            contents = f.read().splitlines()
+        return contents
 
-
-@apply_to_all(is_dir)
-class Directory:
-    def filenames(self, path, nested=False):
-        p = Path()
-
-        # Search sub-directory as well
-        if nested:
-            filepaths = list()
-            for dirpath, dirnames, filenames in os.walk(path):
-                filepaths.extend(filenames)
-            return filepaths
-        # Current directory level only
-        else:
-            return [filename for filename in os.listdir(path) if p.is_file(p.join(path, filename))]
-
-    def filepaths(self, path, nested=False):
-        p = Path()
-
-        # Search sub-directory as well
-        if nested:
-            filepaths = list()
-            for dirpath, dirnames, filenames in os.walk(path):
-                filepaths.extend([p.join(dirpath, filename) for filename in filenames])
-            return filepaths
-        # Current directory level only
-        else:
-            return [p.join(path, filename) for filename in os.listdir(path) if p.is_file(p.join(path, filename))]
-
-    def is_empty(self, path):
-        dirs = os.listdir(path)
-        return len(dirs) == 0
-
-    def remove(self, path):
-        shutil.rmtree(path)
-
-
-@apply_to_all(is_file)
-class File:
-    def basename(self, path: str) -> str:
-        return os.path.basename(path)
-
-    def count_chars(self, path: str) -> int:
-        with open(path, 'r') as infile:
-            count = 0
-            for idx, line in enumerate(infile):
-                count += len(line)
-        return count
-
-    def count_lines(self, path: str) -> int:
-        with open(path, 'r') as infile:
-            for idx, line in enumerate(infile):
-                pass
-        return idx + 1
-
-    def dirname(self, path: str) -> str:
-        return os.path.dirname(path)
-
-    def size(self, path: str) -> int:
-        return os.path.getsize(path)
-
-    def remove(self, path: str):
-        os.remove(path)
-
-
-@apply_to_all(is_file)
-class Read:
-    def all(self, path: str) -> str:
-        with open(path, 'r') as infile:
-            content = infile.read()
-        return content
-
-    def first_n_chars(self, path: str, n: int) -> str:
-        try:
-            with open(path, 'r') as infile:
-                content = infile.read(n)
-            return content
-
-        except Exception as e:
-            logger.error(e)
-
-
-    def first_n_lines(self, path: str, n: int) -> str:
-        try:
-            with open(path, 'r') as infile:
-                content = ''
-                for idx, line in enumerate(infile):
-                    if idx == n:
-                        break
-                    content += line
-            return content
-
-        except Exception as e:
-            logger.error(e)
-
-    def last_n_chars(self, path: str, n: int) -> str:
-        try:
-            with open(path, 'r') as infile:
-                queue = collections.deque(maxlen=n)
-                for idx, line in enumerate(infile):
-                    queue.extend(list(line))
-            return ''.join(queue)
-
-        except Exception as e:
-            logger.error(e)
-
-
-    def last_n_lines(self, path: str, n: int) -> str:
-        try:
-            with open(path, 'r') as infile:
-                queue = collections.deque(maxlen=n)
-                for idx, line in enumerate(infile):
-                    queue.append(line)
-            return ''.join(queue)
-
-        except Exception as e:
-            logger.error(e)
-
-
-    def json(self, path: str) -> dict:
-        with open(path, 'r') as infile:
-            json_obj = json.load(infile)
+    def read_json(self, path: str) -> dict:
+        with open(path, 'r') as f:
+            json_obj = json.load(f)
         return json_obj
 
+    def write_text(self, filepath: str, content: str, mode='a') -> None:
+        with open(filepath, mode=mode) as f:
+            f.write(content)
 
-class Write:
-    def data(self, path: str, data: str, overwrite = True):
-        with open(path, 'w' if overwrite else 'a') as outfile:
-            outfile.write(data)
+    def write_json(self, path: str, data: dict, mode='a', indent=4) -> None:
+        json_obj = json.dumps(data, indent=indent)
+        with open(path, mode=mode) as f:
+            f.write(json_obj)
 
-    def json(self, path: str, data: dict, overwrite = True):
-        json_obj = json.dumps(data, indent=4)
-        with open(path, 'w' if overwrite else 'a') as outfile:
-            outfile.write(json_obj)
+    def zip(self, src: str, dst: str) -> None:
+        with ZipFile(dst, 'w') as z:
+            z.write(src)
+
+    def unzip(self, src: str, dst: str) -> None:
+        with ZipFile(src, 'r') as z:
+            z.extractall(dst)
